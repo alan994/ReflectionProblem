671794a46ff7171767bf2951d28468dcddf22092
Alan Jagar <alan.jagar@hotmail.com>
2019-05-18 19:42:40 +0200

    Add project files.

---
+++ ReflectionTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReflectionTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var request = new MyRequest();
+			IMediator mediator = new Mediator();
+
+			//This works (of course), but I need to call this by reflection. I don't know the type at design time.
+			//var r = mediator.Send(request);
+
+			//I tried this, but it doesn't work
+			var type = request.GetType();
+			var method = mediator.GetType().GetMethod("Send");
+			var generic = method.MakeGenericMethod(type);
+			//Exception
+			var response = generic.Invoke(mediator, new object[] { request });
+
+		}
+	}
+
+	public interface IRequest<out TResponse>
+	{
+
+	}
+
+	public interface IMediator
+	{
+		TResponse Send<TResponse>(IRequest<TResponse> requests);
+	}
+
+	public class MyRequest : IRequest<MyResponse>
+	{
+	}
+
+	public class MyResponse
+	{
+	}
+
+	public class Mediator : IMediator
+	{
+		public TResponse Send<TResponse>(IRequest<TResponse> requests)
+		{
+			Console.WriteLine("Processing...");
+			return default(TResponse);
+		}
+	}
+}
